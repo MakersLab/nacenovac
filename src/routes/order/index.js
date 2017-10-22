ead7e2539306eb82a31a4d4c96bb467868eb01b6
Jakub Zíka <kuba.zika@email.cz>
2017-10-22 13:38:07 +0200

    Add order view with email input box, add styles to it

---
+++ src/routes/order/index.js
@@ -0,0 +1,53 @@
+import { h, Component } from 'preact';
+import style from './style';
+
+
+export default class Order extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      form: {
+        email: '',
+      }
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(e, type) {
+    this.setState({
+      ...this.state,
+      form: {
+        ...this.state.form,
+        [type]: e.target.value,
+      }
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.createOrder(this.state.form.email);
+  }
+
+  render(props, state) {
+    console.log(state);
+    return(
+      <div class={`${style['order']} container`}>
+        <div class="row">
+          <h1>Order</h1>
+        </div>
+        <div class="row">
+          <form onSubmit={this.handleSubmit}>
+            <label>Email:</label>
+            <input
+              type="email"
+              label="email"
+              onChange={(e) => {this.handleInputChange(e, 'email')}}
+              value={state.form.email}/>
+            <input class="button" type="submit" value="Create order"/>
+          </form>
+        </div>
+      </div>);
+  }
+}
