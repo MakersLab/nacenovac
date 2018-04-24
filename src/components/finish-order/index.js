8f47e629a997fc36511686803678e3acc642e537
Jakub Zika <kuba.zika@email.cz>
2018-04-24 11:19:41 +0200

    Add href to link which goes back to homepage

--- src/components/finish-order/index.js
+++ src/components/finish-order/index.js
@@ -12,7 +12,7 @@ export default class FinishOrder extends Component {
       <p class="">
         Děkujeme, za Vaši poptávku. Budeme Vás kontaktovat. Detaily objednávky jsme Vám zaslali na email.
       </p>
-      <a href="">Zpět na hlavní stránku</a>
+      <a className="link" href="https://www.openinnovations.cz/3dtovarna/">Zpět na hlavní stránku</a>
       {/*<a class="button" href="http://3dtovarna.cz/" class={`button two columns offset-by-four ${style['button']}`}>*/}
         {/*OK*/}
       {/*</a>*/}
